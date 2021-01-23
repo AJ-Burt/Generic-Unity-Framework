8e1359ee1f52578f70b8fae0e3f41d5faefcf792
AJ-Burt <38769570+AJ-Burt@users.noreply.github.com>
2021-01-23 14:23:53 -0700

    Initial commit

---
+++ Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,63 @@
+﻿using RPG.Combat;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using RPG.Core;
+
+namespace RPG.Movement
+{
+    public class Mover : MonoBehaviour, IAction
+    {
+
+        [SerializeField] float maxSpeed = 6f;
+
+        NavMeshAgent navMeshAgent;
+        Health health;
+
+        private void Start()
+        {
+            navMeshAgent = GetComponent<NavMeshAgent>();
+            health = GetComponent<Health>();
+        }
+        
+        void Update()
+        {
+
+            navMeshAgent.enabled = !health.IsDead();
+
+            UpdateAnimator();
+        }
+
+        public void StartMoveAction(Vector3 destination, float speedFraction)
+        {
+            GetComponent<ActionScheduler>().StartAction(this);
+            MoveTo(destination, speedFraction);
+        }
+
+        private void UpdateAnimator()
+        {
+            Vector3 velocity = navMeshAgent.velocity;
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+
+            float speed = localVelocity.z;
+
+            GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+        }
+               
+        public void MoveTo(Vector3 destination, float speedFraction)
+        {
+            navMeshAgent.isStopped = false;
+            navMeshAgent.speed = maxSpeed * Mathf.Clamp01(speedFraction);
+            navMeshAgent.destination = destination;
+        }
+        
+
+        public void Cancel()
+        {
+            navMeshAgent.isStopped = true;
+        }
+    }
+
+}
